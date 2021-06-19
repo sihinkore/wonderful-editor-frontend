e0c2d60feaee088ba589eee692575d7dba71fa19
isakamasayuki <roentgenwinter@gmail.com>
2021-06-20 08:54:38 +0900

    登録ページのデザインの実装

---
+++ pages/sign_up.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-card tile flat :class="$style.card">
+    <v-card-title> ユーザー登録 </v-card-title>
+    <v-text-field
+      label="アカウント名"
+      placeholder="お名前"
+      outlined
+    ></v-text-field>
+    <v-text-field
+      label="メールアドレス"
+      placeholder="test@example.com"
+      outlined
+    ></v-text-field>
+    <v-text-field
+      label="パスワード(半角英数字)"
+      placeholder="*******"
+      outlined
+    ></v-text-field>
+    <div class="text-center">
+      <v-btn rounded color="primary" dark block> 登録 </v-btn>
+    </div>
+  </v-card>
+</template>
+<style lang="scss" module>
+.card {
+  width: 600px;
+  margin: auto;
+  padding: 30px 70px;
+}
+</style>
